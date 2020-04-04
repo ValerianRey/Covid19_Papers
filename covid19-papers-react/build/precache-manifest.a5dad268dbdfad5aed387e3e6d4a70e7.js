3ffdfb1222eb43a084d829f361baa86bc140ddbe
Aaron Klotnia <aklotnia@gmail.com>
2020-04-04 19:44:48 -0400

    Added Build File React

---
+++ covid19-papers-react/build/precache-manifest.a5dad268dbdfad5aed387e3e6d4a70e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76bacb77932df4635f0b21eee1ba310",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a3cf39cc4e861c96c23",
+    "url": "/static/css/main.c7ee184f.chunk.css"
+  },
+  {
+    "revision": "cb3669358484ff207dd3",
+    "url": "/static/js/2.e4ff672a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e4ff672a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3cf39cc4e861c96c23",
+    "url": "/static/js/main.a7a0029f.chunk.js"
+  },
+  {
+    "revision": "68389899935138593257",
+    "url": "/static/js/runtime-main.58fd4268.js"
+  },
+  {
+    "revision": "e372b9db1c823d6b3d0b8d3ff950c34f",
+    "url": "/static/media/ColorWheel.e372b9db.png"
+  },
+  {
+    "revision": "19ebd9a2ba056f91ef72bda2af187ada",
+    "url": "/static/media/curvedline.19ebd9a2.png"
+  }
+]);
